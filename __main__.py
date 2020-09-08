bdcb818ce10bec80c71aa2fac3099d9824c840ce
Collins Uzebu <uzebuc@gmail.com>
2020-09-08 20:53:39 +0100

    black-formatting

--- __main__.py
+++ __main__.py
@@ -9,84 +9,101 @@ from core import telegramer
 
 def _validate_input_args(args):
     if not os.path.isdir(args.directory):
-        err_msg = ('A valid directory is required')
+        err_msg = "A valid directory is required"
 
-        print(f'{argparser.prog}: error: {err_msg}', file=sys.stderr)
+        print(f"{argparser.prog}: error: {err_msg}", file=sys.stderr)
         sys.exit()
 
 
 argparser = ArgumentParser(
-    prog='telegramer',
-    description='Rename audio files gotten from Telegram channels',
-    formatter_class=lambda prog: HelpFormatter(prog,width=120,max_help_position=55))
+    prog="telegramer",
+    description="Rename audio files gotten from Telegram channels",
+    formatter_class=lambda prog: HelpFormatter(prog, width=120, max_help_position=55),
+)
 
 
-argparser.add_argument('--version',
-    action='version',
-    version='%(prog)s 1.0')
+argparser.add_argument("--version", action="version", version="%(prog)s 1.0")
 
 group1 = argparser.add_mutually_exclusive_group()
 
-group1.add_argument("-v", "--verbose",
-	help="Show operation output in stdout",
+group1.add_argument(
+    "-v",
+    "--verbose",
+    help="Show operation output in stdout",
     action="store_const",
     dest="log",
-    const=logging.INFO)
+    const=logging.INFO,
+)
 
 
 group1.add_argument(
-    '--debug',
+    "--debug",
     help="Enable debug mode",
     action="store_const",
     dest="log",
     const=logging.DEBUG,
-    default=logging.WARNING)
+    default=logging.WARNING,
+)
 
 group2 = argparser.add_mutually_exclusive_group()
 
-group2.add_argument("-t", "--title",
-	help="Customize file name with your preference",
-    dest="custom_title")
+group2.add_argument(
+    "-t",
+    "--title",
+    help="Customize file name with your preference",
+    dest="custom_title",
+)
 
-argparser.add_argument('-g', '--groupby',
+argparser.add_argument(
+    "-g",
+    "--groupby",
     choices=categorize.group_options,
     required=False,
-    dest='groupby',
-    help=('Specify how the audio files in a directory '
-    	  'are to be grouped.'))
+    dest="groupby",
+    help=("Specify how the audio files in a directory " "are to be grouped."),
+)
 
 
-argparser.add_argument('-r', '--rename',
+argparser.add_argument(
+    "-r",
+    "--rename",
     required=False,
-    dest='rename',
+    dest="rename",
     action="store_true",
-    help=('Select option to begin renaming audio files'))
+    help=("Select option to begin renaming audio files"),
+)
 
-argparser.add_argument('-f', '--folder',
+argparser.add_argument(
+    "-f",
+    "--folder",
     required=False,
-    dest='custom_folder',
-    default='custom_folder',
-    help='Specify a custom directory name. Use this where item '
-         'could not be grouped with the groupby flag.')
+    dest="custom_folder",
+    default="custom_folder",
+    help="Specify a custom directory name. Use this where item "
+    "could not be grouped with the groupby flag.",
+)
 
 
 directory = os.getcwd()
 
-argparser.add_argument('-d', '--directory',
-    dest='directory',
+argparser.add_argument(
+    "-d",
+    "--directory",
+    dest="directory",
     default=directory,
-    help='Specify the directory to perform the '
-         'operation. Default is current directory')
+    help="Specify the directory to perform the "
+    "operation. Default is current directory",
+)
 
 
 args = argparser.parse_args()
 _validate_input_args(args)
 
-logging.basicConfig(level=args.log, format='%(message)s')
+logging.basicConfig(level=args.log, format="%(message)s")
 
 if args.rename:
-	telegramer.main(args)
+    telegramer.main(args)
 
 if args.groupby:
-	result = categorize.GroupFiles().run(args)
-	logging.info(result)
+    result = categorize.GroupFiles().run(args)
+    logging.info(result)
--- core/telegramer.py
+++ core/telegramer.py
@@ -1,4 +1,3 @@
-
 import os, sys, re
 from pathlib import Path
 
@@ -6,112 +5,116 @@ from phrydy import MediaFile
 from phrydy.mediafile import FileTypeError, UnreadableFileError
 from PIL import Image
 
-image_path = r'C:\Users\CollinsUz\Desktop\apple-logo.png'
+image_path = r"C:\Users\CollinsUz\Desktop\apple-logo.png"
 
 
 def delete_media_host(string):
-	if not string:
-		return None
-	substitute = re.compile(r"""
+    if not string:
+        return None
+    substitute = re.compile(
+        r"""
 							(\s*?(?:\[|\(|\{|)?@\w*\s*\w+(?:\)|\]|\})?	#@mention
 							|\s?\|?\|?\-?\s*\#?
 							\[?(?:\w{3}\.)?\w+\.\w{2,3}(?:\.\w{2})?\b\]? 	#website
 							|\[?\s*\bBB\w{0,3}\s*\-?(CHANNEL)?\s*?\-?\s*\w+\s*\]?	#bbm
 							|\s+\|s*
 							|\||\\|\/|\?|\<|\>|\:|\*|)""",
-							re.VERBOSE | re.IGNORECASE)
-	string = re.sub(r'\s+', ' ', string)
-	string = re.sub('\"', '\'', string)
-	string = re.sub(':', ';', string)
-	string = re.sub('/', '-', string)
-	return re.sub(substitute, '', string).strip()
+        re.VERBOSE | re.IGNORECASE,
+    )
+    string = re.sub(r"\s+", " ", string)
+    string = re.sub('"', "'", string)
+    string = re.sub(":", ";", string)
+    string = re.sub("/", "-", string)
+    return re.sub(substitute, "", string).strip()
 
 
 def word_count(string):
-	return len(string.split())
+    return len(string.split())
 
 
 def modify_metatag(media_file):
-	media_file.title = delete_media_host(media_file.title)
-	media_file.artist = delete_media_host(media_file.artist)
-	media_file.album = delete_media_host(media_file.album)
-	media_file.save()
-	return media_file
+    media_file.title = delete_media_host(media_file.title)
+    media_file.artist = delete_media_host(media_file.artist)
+    media_file.album = delete_media_host(media_file.album)
+    media_file.save()
+    return media_file
 
 
 def set_art_metatag(media_file, img):
-	img = Image.open(img).tobytes()
-	media_file.art = img
-	media_file.save()
+    img = Image.open(img).tobytes()
+    media_file.art = img
+    media_file.save()
 
-	return media_file
+    return media_file
 
 
 def get_audio_title(audio_path, default=False, title=False):
-	if default:
-		# Get the original file name
-		return  os.path.splitext(os.path.basename(audio_path))[0]
-	
-	try:
-		media_file = MediaFile(audio_path)
-		media_file = modify_metatag(media_file)
-		
-		if title:
-			return delete_media_host('{}'.format(media_file.title))
-		
-		audio_name = delete_media_host('{} - {}'.format(media_file.artist, media_file.title))
-		return audio_name
-	
-	except (FileTypeError, UnreadableFileError) as e:
-		base = os.path.splitext(os.path.basename(audio_path))[0]
-		print(f'File format not supported: "{base}"')
-		return base
+    if default:
+        # Get the original file name
+        return os.path.splitext(os.path.basename(audio_path))[0]
 
+    try:
+        media_file = MediaFile(audio_path)
+        media_file = modify_metatag(media_file)
+
+        if title:
+            return delete_media_host("{}".format(media_file.title))
+
+        audio_name = delete_media_host(
+            "{} - {}".format(media_file.artist, media_file.title)
+        )
+        return audio_name
+
+    except (FileTypeError, UnreadableFileError) as e:
+        base = os.path.splitext(os.path.basename(audio_path))[0]
+        print(f'File format not supported: "{base}"')
+        return base
 
 
 def rename_audio(audio_name, audio_path, custom=False):
-	audio_ext = os.path.splitext(audio_path)[1]
-	old_audio_name = audio_path
-	if custom:
-		audio_name = custom
-	new_audio_name = os.path.join(os.path.dirname(audio_path), audio_name+audio_ext)
+    audio_ext = os.path.splitext(audio_path)[1]
+    old_audio_name = audio_path
+    if custom:
+        audio_name = custom
+    new_audio_name = os.path.join(os.path.dirname(audio_path), audio_name + audio_ext)
 
-	if 'None' in new_audio_name:
-		print('[-] Empty metatag')
+    if "None" in new_audio_name:
+        print("[-] Empty metatag")
 
-	elif word_count(new_audio_name) > 23:
-		print('[-] Cannot rename | Text too long')
+    elif word_count(new_audio_name) > 23:
+        print("[-] Cannot rename | Text too long")
 
-	else:
-		if not os.path.exists(new_audio_name):
-			os.rename(old_audio_name, new_audio_name)
-			# print(f'[+] Successfully renamed {new_audio_name}')
+    else:
+        if not os.path.exists(new_audio_name):
+            os.rename(old_audio_name, new_audio_name)
+            # print(f'[+] Successfully renamed {new_audio_name}')
 
-	return new_audio_name
-
+    return new_audio_name
 
 
 def main(args):
-	directory = args.directory
+    directory = args.directory
 
-	if args.custom_title:
-		r = input('\nConfirm you want to rename all files in '
-				  'this directory with the same name (y/n): ')
+    if args.custom_title:
+        r = input(
+            "\nConfirm you want to rename all files in "
+            "this directory with the same name (y/n): "
+        )
 
-		r = True if r.strip().lower().startswith('y') else False
+        r = True if r.strip().lower().startswith("y") else False
 
-	for audio in os.listdir(directory):
-		audio_path = os.path.join(directory, audio)
+    for audio in os.listdir(directory):
+        audio_path = os.path.join(directory, audio)
 
-		if os.path.isfile(audio_path):
-			audio_name = get_audio_title(audio_path, title=False)
-			rename_audio(audio_name, audio_path)
+        if os.path.isfile(audio_path):
+            audio_name = get_audio_title(audio_path, title=False)
+            rename_audio(audio_name, audio_path)
 
-			# Use Customized tile
-			if args.custom_title:
-				audio_name = get_audio_title(audio_path, default=True)
-				custom = '{} - {}'.format(audio_name[-3:], args.custom_title)
-				if custom[:3].isdigit() and r:
-					rename_audio(audio_name, audio_path, custom=custom)
-				elif r:
-					rename_audio(audio_name, audio_path, custom=args.custom_title)
+            # Use Customized tile
+            if args.custom_title:
+                audio_name = get_audio_title(audio_path, default=True)
+                custom = "{} - {}".format(audio_name[-3:], args.custom_title)
+                if custom[:3].isdigit() and r:
+                    rename_audio(audio_name, audio_path, custom=custom)
+                elif r:
+                    rename_audio(audio_name, audio_path, custom=args.custom_title)
